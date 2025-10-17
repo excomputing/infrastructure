f09ca806793327937d5be41a487013c6349c3c86
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-17 18:16:11 +0100

    Update src/ecs/task.py: In Progress

--- src/ecs/task.py
+++ src/ecs/task.py
@@ -47,16 +47,32 @@ class Task:
 
         return sum(elements, [])
 
-    def deregister_task_definition(self, definitions: dict):
+    def __delete_task_definitions(self, element: str):
 
-        definitions['family'] = 'FewTokens'
+        try:
+            message = self.__ecs_client.delete_task_definitions(
+                taskDefinitions=[element]
+            )
+            self.__logger.info('%s deleted.', message['taskDefinitions'][0]['family'])
+        except self.__ecs_client.exceptions.ClientException:
+            self.__logger.info('%s does not exist.', element.rsplit(sep='/', maxsplit=1)[1])
+        except botocore.exceptions.ClientError as err:
+            raise err from err
+
+
+    def deregister_task_definition(self, definitions: dict):
 
         elements = self.__inspect(definitions=definitions)
 
-        # `elements` is empty, there are no tasks to deregister
+        # If `elements` is empty, there are no tasks to deregister.
         if not elements:
             self.__logger.info('deregister applicable: false')
             return None
 
-    def __delete_task_definitions(self):
-        pass
+        # Each element is an Amazon Resource Name string, each string includes a task definition family
+        # name, and a revision number.
+        for element in elements:
+            message = self.__ecs_client.deregister_task_definition(
+                taskDefinition=element
+            )
+            self.__delete_task_definitions(element=message['taskDefinition']['taskDefinitionArn'])
