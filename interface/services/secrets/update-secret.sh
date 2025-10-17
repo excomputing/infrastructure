158cff2855010f49806a227eb4b48f1feebd622c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-17 21:39:13 +0100

    Add interface/services/secrets/update-secret.sh

---
+++ interface/services/secrets/update-secret.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+: << 'comment'
+This script updates the keys of the dispatches project.
+comment
+
+path=file://interface/services/secrets
+
+aws secretsmanager update-secret --cli-input-json "$path/update.json" --secret-string "$path/secret-string.json"
