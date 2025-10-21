ed377f643fbfe6755495f0e5735be62b1a361f71
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-22 00:09:36 +0100

    Update src/machines/craft.py: In Progress

--- src/machines/craft.py
+++ src/machines/craft.py
@@ -1,8 +1,16 @@
+import boto3
+
 
 class Craft:
 
-    def __init__(self):
-        pass
+    def __init__(self, connector: boto3.session.Session):
+        """
+
+        :param connector: A boto3 session instance, it retrieves the developer's <default> Amazon
+                          Web Services (AWS) profile details, which allows for programmatic interaction with AWS.
+        """
+
+        self.__ecs_client = connector.client(service_name='stepfunctions')
 
     def describe_state_machine(self, machine: dict):
         pass
