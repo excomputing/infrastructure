9e3b44109571d83095cfc0c18b63e55a8f96d27a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-22 00:04:28 +0100

    Update src/machines/craft.py: In Progress

--- src/machines/craft.py
+++ src/machines/craft.py
@@ -3,3 +3,12 @@ class Craft:
 
     def __init__(self):
         pass
+
+    def describe_state_machine(self, machine: dict):
+        pass
+
+    def create_state_machine(self, machine: dict):
+        pass
+
+    def delete_state_machine(self, machine: dict):
+        pass
