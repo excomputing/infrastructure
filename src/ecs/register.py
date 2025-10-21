00ea11b9eaa985daabda4bcaf48279670c367359
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-21 13:36:01 +0100

    Update src/ecs/register.py: In Progress

--- src/ecs/register.py
+++ src/ecs/register.py
@@ -6,6 +6,7 @@ import botocore.exceptions
 
 import config
 import src.functions.secret
+import src.elements.s3_parameters as s3p
 
 
 class Register:
@@ -13,22 +14,23 @@ class Register:
     https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ecs/client/register_task_definition.html
     """
 
-    def __init__(self, connector: boto3.session.Session):
+    def __init__(self, connector: boto3.session.Session, s3_parameters: s3p.S3Parameters):
         """
 
         :param connector: A boto3 session instance, it retrieves the developer's <default> Amazon
                           Web Services (AWS) profile details, which allows for programmatic interaction with AWS.
+        :param s3_parameters: The overarching S3 parameters settings of this project, e.g., region code
+                              name, buckets, etc.
         """
 
         self.__ecs_client = connector.client(service_name='ecs')
+        self.__s3_parameters = s3_parameters
 
+        # Configurations
         self.__configurations = config.Config()
-        self.__secret = src.functions.secret.Secret(connector=connector)
 
-        # Logging
-        logging.basicConfig(level=logging.INFO, format='\n\n%(message)s\n%(asctime)s.%(msecs)03d\n',
-                            datefmt='%Y-%m-%d %H:%M:%S')
-        self.__logger = logging.getLogger(__name__)
+        # Secrets
+        self.__secret = src.functions.secret.Secret(connector=connector)
 
     def exc(self, definitions: dict):
         """
@@ -37,7 +39,9 @@ class Register:
         :return:
         """
 
-        self.__logger.info(definitions.get('family'))
+        container_definitions: list[dict] = definitions.get('containerDefinitions')
+        for c in container_definitions:
+            c['logConfiguration']['options']['awslogs-region'] = self.__s3_parameters.region_name
 
 
         try:
@@ -46,8 +50,7 @@ class Register:
                 taskRoleArn=self.__secret.exc(secret_id=self.__configurations.project_key_name, node=''),
                 executionRoleArn=self.__secret.exc(secret_id=self.__configurations.project_key_name, node=''),
                 networkMode=definitions.get('networkMode'),
-                containerDefinitions=[]
+                containerDefinitions=container_definitions
             )
         except botocore.exceptions.ClientError as err:
             raise err from err
-
