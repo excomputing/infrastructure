4fe42db28f50023cbf1d4b9ad0484b53ff6b421d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-12-07 23:30:43 +0000

    Update src/eks/script.sh

--- src/eks/script.sh
+++ src/eks/script.sh
@@ -3,8 +3,20 @@
 : << 'comment'
 If an ARM system, the processor architecture is either `arm64`, `armv6` or `armv7`; instead
 of `amd64`.
+
+If the project machine has an installation of Docker Desktop, it is quite possible that kubectl is installed.  If not
+
+```bash
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.5/2025-11-13/bin/linux/amd64/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.5/2025-11-13/bin/linux/amd64/kubectl.sha256
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+```
 comment
 
+
+# eksctl
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_Linux_amd64.tar.gz"
 curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep Linux_amd64 | sha256sum --check
 tar -xzf eksctl_Linux_amd64.tar.gz -C /tmp && rm eksctl_Linux_amd64.tar.gz
