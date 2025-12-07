fe07cd11d2198c4f1a6701f9397d5d7c70603aa3
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-12-07 23:42:41 +0000

    Update src/eks/script.sh

--- src/eks/script.sh
+++ src/eks/script.sh
@@ -16,7 +16,7 @@ echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
 comment
 
 
-# eksctl
+# Unload and install `eksctl`
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_Linux_amd64.tar.gz"
 curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep Linux_amd64 | sha256sum --check
 tar -xzf eksctl_Linux_amd64.tar.gz -C /tmp && rm eksctl_Linux_amd64.tar.gz
